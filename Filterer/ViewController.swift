96625548d89b81de8a55bf7782babef783d6211b
markus <markus@torpvret.se>
2015-11-03 22:01:27 +0100

    Initial Commit

---
+++ Filterer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Filterer
+//
+//  Created by Markus Torpvret on 2015-11-03.
+//  Copyright © 2015 Markus Torpvret. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
